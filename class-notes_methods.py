2f4013e8993f9532068ea74e8a73cbb25c8a4fe2
Ryan-W-Harrison <harrison.ryan.w@gmail.com>
2023-10-14 10:17:05 -0400

    add notes

--- class-notes_methods.py
+++ class-notes_methods.py
@@ -52,5 +52,25 @@ my_circle = Circle()
 my_circle.get_circumference()
 my_circle.area
 
+class Animal():
+    def __init__(self):
+        print("Animal Created")
+    
+    def who_am_i(self):
+        print("I am an animal")
+    def eat(self):
+        print("I am eating")
 
+myanimal = Animal()
         
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog Created")
+
+    def who_am_i(self):
+        print("I am a dog")
+
+mydog = Dog()
+
+mydog.who_am_i()
